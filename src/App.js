09e2b21b8bf51e1b75bf0d96d9da8096c4fb3a1e
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-21 22:59:17 +0200

    added dropdown menu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import DropdownMenu from "./DropdownMenu";
 
 import UserIcon from "./UserIcon";
 import ReactDOM from "react-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { Dropdown } from "bootstrap";
 
 library.add(fab, faCircleUser);
 
@@ -13,6 +15,7 @@ function App() {
     <div>
       <header className="background-image"></header>
       <UserIcon />
+      <DropdownMenu />
     </div>
   );
 }
---
+++ src/DropdownMenu.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DropdownMenu() {
+  return <h1>Hello from dropdown</h1>;
+}
