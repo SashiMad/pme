a68a7a53ee437bcdc14540681909f890d5bbe4dd
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-21 11:46:02 +0200

    positioned header, added responsive feature

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          <img src={Header} alt="logo" />
+          <img src={Header} className="header img-fluid" alt="logo" />
         </h1>
       </header>
     </div>
