0a73ab2ee13cf2bef0525581f2f6987898026930
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-21 13:43:11 +0200

    reset header as background image in css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
-import UserIcon from "./UserIcon";
-import Header from "./images/Header.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          <img src={Header} className="header img-fluid" alt="logo" />
-        </h1>
-        <h2>
-          <UserIcon />
-        </h2>
-      </header>
+    <div>
+      <header className="background-image"></header>
     </div>
   );
 }
--- src/UserIcon.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserIcon() {
-  return <h2> Hello from User Icon</h2>;
-}
