d884fd25868e747edb9466b9d4f2159ed878a5af
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-21 15:47:04 +0200

    imported fontawesome as npms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import UserIcon from "./UserIcon";
+import ReactDOM from "react-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCircleUser);
 
 function App() {
   return (
     <div>
       <header className="background-image"></header>
+      <UserIcon />
     </div>
   );
 }
---
+++ src/UserIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function UserIcon() {
+  return (
+    <div>
+      <FontAwesomeIcon icon="fa-solid fa-circle-user" size="lg" />
+    </div>
+  );
+}
