cebb8c08bcc894cfb64c78bfb992bf0e24ba90d3
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-21 23:01:31 +0200

    cleaned up app.js, deleted unused imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./App.css";
 import DropdownMenu from "./DropdownMenu";
-
 import UserIcon from "./UserIcon";
-import ReactDOM from "react-dom";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import { Dropdown } from "bootstrap";
-
-library.add(fab, faCircleUser);
 
 function App() {
   return (
