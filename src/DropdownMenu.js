240593e8c13474e3f52e2ef66021742c1ab5b91b
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-22 10:29:32 +0200

    imported bootstrap 5 JS bundle, fixed bootstrap JS functionality

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function DropdownMenu() {
   return (
     <div className="dropdown">
-      <button
+      <Button
         className="btn btn-secondary dropdown-toggle"
         type="button"
         id="dropdownMenuButton"
-        data-toggle="dropdown"
+        data-bs-toggle="dropdown"
         aria-haspopup="true"
         aria-expanded="false"
       >
@@ -18,11 +19,36 @@ export default function DropdownMenu() {
           className="envelope-icon"
         ></FontAwesomeIcon>
         E-Mail-Benachrichtigungen verwalten
-      </button>
+      </Button>
       <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
         <a className="dropdown-item" href="/">
           Action
         </a>
+      </div>
+      <div class="dropdown show">
+        <a
+          class="btn btn-secondary dropdown-toggle"
+          href="#"
+          role="button"
+          id="dropdownMenuLink"
+          data-bs-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Dropdown link
+        </a>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <a class="dropdown-item" href="#">
+            Action
+          </a>
+          <a class="dropdown-item" href="#">
+            Another action
+          </a>
+          <a class="dropdown-item" href="#">
+            Something else here
+          </a>
+        </div>
       </div>
     </div>
   );
