872e502799ce708f53fe9c9673041e8e4522b4a7
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-22 16:25:28 +0200

    created password form component

--- src/PopupContent.js
+++ src/PopupContent.js
@@ -1,5 +1,23 @@
 import React from "react";
+import PasswordForm from "./PasswordForm";
 
 export default function PopupContent() {
-  return <div>Hello from popup</div>;
+  return (
+    <div>
+      <div class="card">
+        <div class="card-header">Featured</div>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <PasswordForm />
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
