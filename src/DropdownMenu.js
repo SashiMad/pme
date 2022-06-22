481deb2ab7f1c5bee5bf8a0d2df4aefb32694abf
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-22 09:07:13 +0200

    added dropdown button with fontawesome icon

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function DropdownMenu() {
-  return <h1>Hello from dropdown</h1>;
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="envelope-icon"
+        ></FontAwesomeIcon>
+        E-Mail-Benachrichtigungen verwalten
+      </button>
+      <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a className="dropdown-item" href="/">
+          Action
+        </a>
+      </div>
+    </div>
+  );
 }
