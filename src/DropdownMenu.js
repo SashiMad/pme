00fee52cf9cae4825867082aba2de13cf008bc84
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-22 14:18:04 +0200

    added fontawesome icons to the accordion

--- src/DropdownMenu.js
+++ src/DropdownMenu.js
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
-
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleXmark,
+  faEnvelope,
+  faLock,
+  faAt,
+  faTableList,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function DropdownMenu() {
   return (
-    <div className="App">
+    <div className="DropdownMenu">
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
@@ -17,7 +22,11 @@ export default function DropdownMenu() {
               aria-expanded="true"
               aria-controls="collapseOne"
             >
-              Accordion Item #1
+              <FontAwesomeIcon
+                icon={faEnvelope}
+                className="envelope-icon"
+              ></FontAwesomeIcon>
+              E-Mail-Benachrichtigungen verwalten
             </button>
           </h2>
           <div
@@ -26,16 +35,7 @@ export default function DropdownMenu() {
             aria-labelledby="headingOne"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
+            <div className="accordion-body"></div>
           </div>
         </div>
         <div className="accordion-item">
@@ -48,7 +48,12 @@ export default function DropdownMenu() {
               aria-expanded="false"
               aria-controls="collapseTwo"
             >
-              Accordion Item #2
+              {" "}
+              <FontAwesomeIcon
+                icon={faAt}
+                className="at-icon"
+              ></FontAwesomeIcon>
+              E-Mail-Adresse ändern
             </button>
           </h2>
           <div
@@ -79,7 +84,12 @@ export default function DropdownMenu() {
               aria-expanded="false"
               aria-controls="collapseThree"
             >
-              Accordion Item #3
+              {" "}
+              <FontAwesomeIcon
+                icon={faLock}
+                className="lock-icon"
+              ></FontAwesomeIcon>
+              Passwort ändern
             </button>
           </h2>
           <div
@@ -88,16 +98,61 @@ export default function DropdownMenu() {
             aria-labelledby="headingThree"
             data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classNamees that we use to style each element. These classNamees
-              control the overall appearance, as well as the showing and hiding
-              via CSS transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
+            <div className="accordion-body"></div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo"
+              aria-expanded="false"
+              aria-controls="collapseTwo"
+            >
+              {" "}
+              <FontAwesomeIcon
+                icon={faTableList}
+                className="list-icon"
+              ></FontAwesomeIcon>
+              Onlinedarstellung verwalten
+            </button>
+          </h2>
+          <div
+            id="collapseTwo"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body"></div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseThree"
+              aria-expanded="false"
+              aria-controls="collapseThree"
+            >
+              {" "}
+              <FontAwesomeIcon
+                icon={faCircleXmark}
+                className="xmark-icon"
+              ></FontAwesomeIcon>
+              Benutzerkonto löschen
+            </button>
+          </h2>
+          <div
+            id="collapseThree"
+            className="accordion-collapse collapse"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
+          >
+            <div className="accordion-body"></div>
           </div>
         </div>
       </div>
