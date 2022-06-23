77816e57acf5e905967d968b9d786c3790b3b3ff
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-23 18:25:02 +0200

    transferred dropdownmenu content into new accordion

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import EmailButtonPopup from "./EmailButtonPopup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faEnvelope,
+  faLock,
+  faAt,
+  faTableList,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function AccordionTry() {
   return (
     <div>
       <Accordion>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>
+            <FontAwesomeIcon
+              icon={faEnvelope}
+              className="envelope-icon"
+            ></FontAwesomeIcon>
+            E-Mail-Benachrichtigungen verwalten
+          </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -19,7 +34,70 @@ export default function AccordionTry() {
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>
+            <FontAwesomeIcon icon={faAt} className="at-icon"></FontAwesomeIcon>
+            E-Mail-Adresse ändern
+          </Accordion.Header>
+          <Accordion.Body>
+            <div>
+              {" "}
+              <p>
+                Du hast eine neue E-Mail-Adresse? Hier kannst du jederzeit deine
+                E-Mail-Adresse ändern.
+              </p>{" "}
+            </div>
+            <div>
+              {" "}
+              <p>Deine aktuelle E-Mail-Adresse:</p>{" "}
+              <strong>devet@familienservice.de</strong>
+            </div>
+            <EmailButtonPopup />
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            <FontAwesomeIcon
+              icon={faLock}
+              className="lock-icon"
+            ></FontAwesomeIcon>
+            Passwort ändern
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            <FontAwesomeIcon
+              icon={faTableList}
+              className="list-icon"
+            ></FontAwesomeIcon>
+            Onlinedarstellung verwalten
+          </Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="4">
+          <Accordion.Header>
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className="xmark-icon"
+            ></FontAwesomeIcon>{" "}
+            Benutzerkonto löschen
+          </Accordion.Header>
           <Accordion.Body>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
