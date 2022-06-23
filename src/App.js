1a055818b4f3875a7e8fdd806ef1b9b81ff62cb2
Sashi <103021999+Sashicake@users.noreply.github.com>
2022-06-23 18:16:33 +0200

    created new collapsible accordion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import DropdownMenu from "./DropdownMenu";
 import UserIcon from "./UserIcon";
+import Accordion from "./Accordion";
 
 function App() {
   return (
     <div>
       <header className="background-image"></header>
       <UserIcon />
+      <Accordion />
       <DropdownMenu />
     </div>
   );
